0dbbbc5ba052db18087acfc90ce8e9b2226d5797
Sophia Shuvar <45092554+sophiashuv@users.noreply.github.com>
2020-01-14 22:42:09 +0200

    Update Fraction.java

--- Lesson13/src/task1/Fraction.java
+++ Lesson13/src/task1/Fraction.java
@@ -3,7 +3,6 @@ package task1;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class Fraction {
 	ArrayList<Deputy> fraction = new ArrayList<Deputy>();
 	private String fractionName;
@@ -18,45 +17,45 @@ public class Fraction {
 	    }
 	
 	public Deputy getDeputy(String deputyName) {
-		Deputy is = new Deputy();
-		for (Deputy i :fraction) if (i.getDeputyName() == deputyName) is = i;
-		return is;	
-		}
+	    Deputy is = new Deputy();
+	    for (Deputy i :fraction) if (i.getDeputyName() == deputyName) is = i;
+	    return is;	
+	}
 	
 	public void inputFraction() {
-		myObj = new Scanner(System.in);
+	    myObj = new Scanner(System.in);
 	    System.out.println("Enter fractionName: ");
 	    fractionName = myObj.nextLine();
-	    }
+	}
 	
 	public String getFractionName() {
-		return fractionName;
-		}
+	    return fractionName;
+	}
 	
 	public void addDeputy(Deputy deputy) {
-		fraction.add(deputy);
-		}
+	    fraction.add(deputy);
+	}
 	
 	public void deleteDeputy(String deputyName){
-		fraction.remove(getDeputy(deputyName));
-		}
+	    fraction.remove(getDeputy(deputyName));
+	}
 	
 	public void printBribers(){
 	    for (Deputy d :fraction) if(d.isBriber()) System.out.println(d);
-	    }
+	}
 	
 	public void printMaxBriber() {
-		Deputy maxBribe = new Deputy();
+	    Deputy maxBribe = new Deputy();
 	    for (Deputy i :fraction) if (maxBribe.getBribeSize() < i.getBribeSize()) maxBribe = i;
 	    System.out.println("Max briber: \n" + maxBribe.toString());
-	    }
+	}
 	
 	public void printAll() {
-		System.out.println("All deputies in fraction " + fractionName + ":\n");
-		for (Deputy i :fraction) System.out.println(i.toString());
-	    }
+	    System.out.println("All deputies in fraction " + fractionName + ":\n");
+	    for (Deputy i :fraction) System.out.println(i.toString());
+	}
 	
 	public void clearFraction(){
 	    fraction.clear();
-	    }
+	}
 }
